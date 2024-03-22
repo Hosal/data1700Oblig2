062a239338c285e5824dd940b995ac9326c07ea9
Gunilla Hovden Saltnes <143340451+Hosal@users.noreply.github.com>
2024-03-22 12:52:54 +0100

    Starting classes

--- src/main/java/com/example/oblig2/ArrayClassThing.java
+++ src/main/java/com/example/oblig2/ArrayClassThing.java
@@ -1,4 +1,28 @@
 package com.example.oblig2;
 
+import java.util.ArrayList;
+
+class TicketPurchase {
+    private String choose;
+    private String amount;
+    private String fName;
+    private String lName;
+    private String tel;
+    private String email;
+
+    public TicketPurchase (String choose, String amount, String fName, String lName, String tel, String email){
+        this.choose = choose;
+        this.amount= amount;
+        this.fName = fName;
+        this.lName = lName;
+        this.tel = tel;
+        this.email = email;
+    }
+}
+
 public class ArrayClassThing {
+    public static void main(String[] args) {
+        ArrayList<TicketPurchase> boughtTicket = new ArrayList<>();
+
+    }
 }
